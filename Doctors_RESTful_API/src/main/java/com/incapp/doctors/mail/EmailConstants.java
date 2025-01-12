b9212db0b27b6e3082d788e25257e2897d9bfdaa
Ayush kushwaha <140896995+ayushkushwaha0507@users.noreply.github.com>
2025-01-13 02:25:22 +0530

    Update EmailConstants.java

--- Doctors_RESTful_API/src/main/java/com/incapp/doctors/mail/EmailConstants.java
+++ Doctors_RESTful_API/src/main/java/com/incapp/doctors/mail/EmailConstants.java
@@ -1,6 +1,6 @@
 package com.incapp.doctors.mail;
 
 public class EmailConstants {
-    public static final String ADMIN_EMAIL = "shivamkushwaha2627@gmail.com";
-    public static final String ADMIN_PASSWORD = "sdxo smqt fhrv wuhk"; // Ensure this is stored securely in production
+    public static final String ADMIN_EMAIL = "Your Emial ID";
+    public static final String ADMIN_PASSWORD = "Your Password "; // Ensure this is stored securely in production
 }
